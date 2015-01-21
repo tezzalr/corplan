53ebb82a918ea47b2c5d590481f6c0285ff94000
Tezza LR <tezzalr@gmail.com>
2015-01-21 14:50:09 +0700

    outlook

--- application/views/general/outlook.php
+++ application/views/general/outlook.php
@@ -9,33 +9,42 @@
 		background-color:#f5f5f5 !important;
 		color:gray !important;
 	}
+	a{
+		color:grey;
+	}
 </style>
 <div id="" class="container no_pad" style="height:86%">
 	<div id="seven-sector" style="padding:0 35px 35px 35px; color:grey;">
 		<h3 style="margin-top:15px; padding-top:15px;">7 Sectors Solutions Corplan</h3><hr>
 		<div class="seven">
 			<div><img src="<?php echo base_url()?>assets/img/sector/fmcg.png" style="height:40px"> FMCG<hr style="margin:10px"></div>
-			<div>Makanan dan Minuman</div><div>Rokok</div>
+			<div><a href="<?php echo base_url()?>assets/other/sector/Makanan Minuman.pdf">Makanan dan Minuman</a></div>
+			<div><a href="<?php echo base_url()?>assets/other/sector/Rokok.pdf">Rokok</a></div>
 		</div>
 		<div class="seven">
 			<div><img src="<?php echo base_url()?>assets/img/sector/telco.png" style="height:40px"> Telco<hr style="margin:10px"></div>
-			<div>Telekomunikasi</div>
+			<div><a href="<?php echo base_url()?>assets/other/sector/Telekomunikasi.pdf">Telekomunikasi</a></div>
 		</div>
 		<div class="seven">
 			<div><img src="<?php echo base_url()?>assets/img/sector/plant.png" style="height:40px"> Plantation<hr style="margin:10px"></div>
-			<div>Gula</div><div>Kakao</div><div>Karet</div><div>Kopi</div><div>Pulp dan Kertas</div>
+			<div><a href="<?php echo base_url()?>assets/other/sector/Gula.pdf">Gula</a></div>
+			<div><a href="<?php echo base_url()?>assets/other/sector/Kakao.pdf">Kakao</a></div>
+			<div><a href="<?php echo base_url()?>assets/other/sector/Karet.pdf">Karet</a></div>
+			<div><a href="<?php echo base_url()?>assets/other/sector/Kopi.pdf">Kopi</a></div>
+			<div><a href="<?php echo base_url()?>assets/other/sector/Pulp dan Kertas.pdf">Pulp dan Kertas</a></div>
 		</div>
 		<div class="seven">
 			<div><img src="<?php echo base_url()?>assets/img/sector/oil.png" style="height:40px"> Oil&Gas<hr style="margin:10px"></div>
-			<div>Minyak</div><div>Gas</div>
+			<div><a href="<?php echo base_url()?>assets/other/sector/Minyak.pdf">Minyak</a></div>
+			<div><a href="<?php echo base_url()?>assets/other/sector/Gas.pdf">Gas</a></div>
 		</div>
 		<div class="seven">
 			<div><img src="<?php echo base_url()?>assets/img/sector/health.png" style="height:40px"> Healthcare<hr style="margin:10px"></div>
-			<div>Farmasi</div>
+			<div><a href="<?php echo base_url()?>assets/other/sector/Farmasi.pdf">Farmasi</a></div>
 		</div>
 		<div class="seven">
 			<div><img src="<?php echo base_url()?>assets/img/sector/port.png" style="height:40px"> Ports<hr style="margin:10px"></div>
-			<div>Transportasi Laut</div>
+			<div><a href="<?php echo base_url()?>assets/other/sector/Transportasi Laut.pdf">Transportasi Laut</a></div>
 		</div>
 		<div class="seven">
 			<div><img src="<?php echo base_url()?>assets/img/sector/gov.png" style="height:40px"> Government<hr style="margin:10px"></div>
